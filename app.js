ef5b07120662ff8d1afd84979a425962067c2b7d
Keaton Okkonen <keaton.okkonen@gmail.com>
2015-08-15 17:41:54 +1000

    initial heroku push

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -11,6 +12,13 @@ app.use(express.static(__dirname + '/public'));
 app.use('/', require('./routes/index')());
 app.use('/profile', require('./routes/profile')());
 app.use('/fight', require('./routes/fight')());
+
+app.use(stormpath.init(app, {
+  apiKeyId:     process.env.STORMPATH_API_KEY_ID,
+  apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
+  secretKey:    process.env.STORMPATH_SECRET_KEY,
+  application:  process.env.STORMPATH_URL,
+}));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
